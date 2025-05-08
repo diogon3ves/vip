65a7d2b970526188099426936356d35246f7facd
Diogo Neves <48537252+diogon3ves@users.noreply.github.com>
2025-05-08 00:07:04 -0300

    Update subscriptions.ts

--- pages/api/subscriptions.ts
+++ pages/api/subscriptions.ts
@@ -5,25 +5,24 @@ const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    const { usuarioId, plano, valor } = req.body;
+    const { usuarioId, valor } = req.body;
 
-    if (!usuarioId || !plano || !valor) {
-      return res.status(400).json({ erro: "Dados obrigatórios ausentes." });
+    if (!usuarioId || !valor) {
+      return res.status(400).json({ erro: "Campos obrigatórios ausentes." });
     }
 
     try {
-      const sub = await prisma.assinatura.create({
+      const saque = await prisma.saque.create({
         data: {
           usuarioId,
-          plano,
           valor: parseFloat(valor)
-        },
+        }
       });
 
-      return res.status(201).json(sub);
+      return res.status(201).json(saque);
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ erro: "Erro ao registrar assinatura." });
+      return res.status(500).json({ erro: "Erro ao registrar saque." });
     }
   } else {
     return res.status(405).json({ erro: "Método não permitido." });
