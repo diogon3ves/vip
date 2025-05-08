7e0dabb132aa4acd6cebf8e4ddc3ea0fff0dc178
Diogo Neves <48537252+diogon3ves@users.noreply.github.com>
2025-05-08 15:32:36 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,14 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // para evitar múltiplas instâncias em hot-reload
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  global.prisma ??
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
-  });
+    log: ['query', 'error', 'warn'],
+  })
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
