d8fd17fccc95ff1a48688dd3bdd03724ece18bb6
Diogo Neves <48537252+diogon3ves@users.noreply.github.com>
2025-05-08 15:34:20 -0300

    Update cadastro.ts

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
 
-const prisma = new PrismaClient()
+import { prisma } from '@/lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
